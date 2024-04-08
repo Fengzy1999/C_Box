a30e20d03e48fa4d4e3dce2124b4ed9e828c3800
Fengzy1999 <70766870+Fengzy1999@users.noreply.github.com>
2024-04-08 16:04:13 +0800

    Update 000-A-box.c

--- 000-A-box.c
+++ 000-A-box.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-# include<math.h> //数学函数
-# include<stdlib.h> //包含system()函数
-#include <stdbool.h> //包含布尔函数的头文件
+#include<math.h> //数学函数
+#include<stdlib.h> //包含system()函数
+#include<stdbool.h> //包含布尔函数的头文件
 #include<malloc.h> //动态申请内存空间 malloc()函数
 #include<string.h> //标准文字输出 strcpy()函数
 
